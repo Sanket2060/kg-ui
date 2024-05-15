d70a875339fa651db3c46646504af51b831bd62f
Sanket <sanketkarki2060@gmail.com>
2024-05-15 14:59:32 +0545

    feat:popups components ui done

--- src/components/PopupLogin.jsx
+++ src/components/PopupLogin.jsx
@@ -10,32 +10,32 @@ const PopupLogin = ({ isOpen, onClose }) => {
         <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 font-Poppins">
           {/* yo line le kasari purai select garexa?? */}
 
-          <div className="bg-white relative rounded-lg py-14 w-[60%] h-[95%] flex flex-col items-center font-medium">
+          <div className="bg-white relative rounded-lg py-14 px-14 w-[90%] md:w-[60%] h-[95%] flex flex-col items-center font-medium">
             <div className="flex justify-end absolute right-5 top-5 hover:cursor-pointer" onClick={onClose}>
               <img src={cross} className="w-4 h-4" alt="" srcset="" />
             </div>
             <img src={kaagazpatralogo} className="w-96 h-34" alt="" srcset="" />
             <div className="text-[#6361DC] text-base mt-1 font-semibold">Login your account</div>
-            <form className="flex flex-col items-center mt-6 mb-6">
-              <div className="email">
+            <form className="flex flex-col items-center mt-6 mb-6 w-full">
+              <div className="email w-full">
                 <div>Email Address</div>
                 <Input
                   placeholder="sandeshghimire202@gmail.com"
-                  special="w-[643px] h-16"
+                  special="w-full h-16"
                   name="email"
                 />
               </div>
               
-              <div className="password">
+              <div className="password w-full">
                 <div>password</div>
                 <Input
                   placeholder="*************"
-                  special="w-[643px] h-16"
+                  special="w-full h-16"
                   name="password"
                 />
               <div className="text-[#6361DC] font-semibold mb-6">Forget password?</div>
               </div>
-              <Button text="Login" special="w-80 h-16" />
+              <Button text="Login" special="largemobiles:w-80 mobile:w-60 h-16" />
             </form>
             <div className="font-semibold mb-10">
               Not Register Yet?{" "}
--- src/components/PopupRegister.jsx
+++ src/components/PopupRegister.jsx
@@ -10,66 +10,66 @@ const PopupRegister = ({ isOpen, onClose }) => {
         <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 font-Poppins">
           {/* yo line le kasari purai select garexa?? */}
 
-          <div className="bg-white relative rounded-lg w-[60%] h-[95%] flex flex-col items-center">
+          <div className="bg-white relative rounded-lg w-[90%] md:w-[60%] h-[95%] px-14 flex flex-col items-center overflow-scroll">
             <div className="flex justify-end absolute right-5 top-5 hover:cursor-pointer" onClick={onClose}>
               <img src={cross} className="w-4 h-4" alt="" srcset=""/>
             </div>
             <img src={kaagazpatralogo} className="w-96 h-34" alt="" srcset="" />
             <div className="text-[#6361DC] text-base">Register new account</div>
-            <form className="flex flex-col items-center mb-6">
-              <div className="name flex">
-                <div className="firstname mr-40">
+            <form className="flex flex-col items-center mb-6 w-full">
+              <div className="name flex flex-col sm:flex-row justify-between w-full">
+                <div className="firstname w-full sm:w-[40%] ">
                   <div>First Name</div>
                   <Input
                     placeholder="Sandesh"
-                    special="w-60 h-16"
+                    special="w-full  h-16"
                     name="firstname"
                   />
                 </div>
-                <div className="lastname">
+                <div className="lastname w-full sm:w-[40%]">
                   <div>Last Name</div>
                   <Input
                     placeholder="Ghimire"
-                    special="w-60 h-16"
+                    special="w-full h-16"
                     name="lastname"
                   />
                 </div>
               </div>
-              <div className="email">
+              <div className="email w-full">
                 <div>Email Address</div>
                 <Input
                   placeholder="sandeshghimire202@gmail.com"
-                  special="w-[643px] h-16"
+                  special="w-full h-16"
                   name="email"
                   type="email"
                 />
               </div>
-              <div className="mobile">
+              <div className="mobile w-full">
                 <div>Mobile No.</div>
                 <Input
                   placeholder="+9779862383881"
-                  special="w-[643px] h-16"
+                  special="w-full h-16"
                   name="mobile"
                 />
               </div>
-              <div className="password">
+              <div className="password w-full">
                 <div>password</div>
                 <Input
                   placeholder="**********"
-                  special="w-[643px] h-16"
+                  special="w-full h-16"
                   name="password"
                 />
               </div>{" "}
-              <div className="confirmpassword">
+              <div className="confirmpassword w-full">
                 <div>confirm Password</div>
                 <Input
                   placeholder="***********"
-                  special="w-[643px] h-16"
+                  special="w-full h-16"
                   name="confirmpassword"
                   type="password"
                 />
               </div>
-              <Button text="Register" special="w-80 h-16" />
+              <Button text="Register" special="w-80 h-16 largemobiles:w-80 mobile:w-60" />
             </form>
             <div className="font-semibold mb-10">Already Have an account ? <span className="text-[#6361DC]"> Log in</span></div>
           </div>
