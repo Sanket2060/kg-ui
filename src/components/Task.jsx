38738beb862b5f951ad90c058edfa9a60a20a866
Sanket <sanketkarki2060@gmail.com>
2024-08-18 13:22:17 +0545

    fixed image at lekhapadi reloading the app

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 function Task({ taskimage, text }) {
   return (
-    <Link to={`/dashboard/generateDocument/${text}`}>
-    <div className="flex flex-col justify-center items-center hover:cursor-pointer">
-      <div className="w-[5.6rem] h-[5.6rem]  border-[1px] mb-2 border-[#E2E7ED] rounded-xl flex justify-center items-center">
-        <img src={taskimage} alt="" className="w-7 h-7" />
-      </div>
-      <div className="text-center">{text}</div>
-    </div>
-    </Link>
+    <>
+      {text ? (
+        <Link to={`/dashboard/${text}`}>
+          <div className="flex flex-col justify-center items-center hover:cursor-pointer">
+            <div className="w-[5.6rem] h-[5.6rem]  border-[1px] mb-2 border-[#E2E7ED] rounded-xl flex justify-center items-center">
+              <img src={taskimage} alt="" className="w-7 h-7" />
+            </div>
+            <div className="text-center">{text}</div>
+          </div>
+        </Link>
+      ) : (
+        <div className="flex flex-col justify-center items-center hover:cursor-pointer">
+          <div className="w-[5.6rem] h-[5.6rem]  border-[1px] mb-2 border-[#E2E7ED] rounded-xl flex justify-center items-center">
+            <img src={taskimage} alt="" className="w-7 h-7" />
+          </div>
+          <div className="text-center">{text}</div>
+        </div>
+      )}
+    </>
   );
 }
 
